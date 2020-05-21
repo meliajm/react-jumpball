f8c3ff32327e50753a48c329f25a450f0be5989f
github username <meliajm@gmail.com>
2020-05-20 17:23:31 -0700

    added feature for basketball to appear on random spot

--- src/App.js
+++ src/App.js
@@ -1,13 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Stage, Layer, Circle, Line} from 'react-konva'
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends React.Component {
+    
+    state = {
+      x: this.getRandomInt(window.innerWidth-20),
+      y: this.getRandomInt(window.innerHeight-20),
+      fill: "white",
+      radius: 20,
+      abled: "able",
+      stroke: "white",
+      // onClick: this.consoleLogNoControl
+    }
+
+    getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    // consoleLogNoControl = () => {
+    //   console.log('not it')
+    // }
+
+    controlMic = () => {
+      console.log("you've got the mic")
+      // this will turn mic on and off for one user
+      // document.querySelector('.U26fgb.JRY2Pb').click()
+
+    }
+    
+    handleClick = (event) => {
+      const xPos = this.getRandomInt(window.innerWidth-this.state.radius)
+      const yPos = this.getRandomInt(window.innerHeight-this.state.radius)
+      
+      this.setState({
+       x: xPos,
+       y: yPos,
+       fill: "orange",
+       abled: "disabled",
+       stroke: "black",
+       onClick: this.controlMic
+      })
+    }
+
+    render() {
+
+      return (
+        <div className="App">
       <h1>Jumpball</h1>
-    </div>
-  );
-}
-
-export default App;
+      <button
+      className={this.state.abled}
+      onClick={this.handleClick}>
+      Start Game
+      </button>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+      <Circle
+            x={this.state.x}
+            y={this.state.y}
+            radius={this.state.radius}
+            fill={this.state.fill}
+            onClick={this.state.onClick}
+      />
+      <Line
+            x={this.state.x}
+            y={this.state.y-20}
+            points={[0, 0, 0, 40]}
+            stroke={this.state.stroke}
+            onClick={this.state.onClick}
+          />
+      <Line
+            x={this.state.x}
+            y={this.state.y-20}
+            points={[-18, 10, -8, 20, -18, 30]}
+            tension={0.5}
+            stroke={this.state.stroke}
+            onClick={this.state.onClick}
+          />
+      <Line
+            x={this.state.x+35}
+            y={this.state.y-20}
+            points={[-18, 10, -28, 20, -18, 30]}
+            tension={0.5}
+            stroke={this.state.stroke}
+            onClick={this.state.onClick}
+          />
+      </Layer>
+      </Stage>
+      </div>
+      );
+    }
+  }
